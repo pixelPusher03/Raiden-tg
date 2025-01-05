21f74341b068bd8fdf9e73a442df55876022bbb9
ᴘɪxᴇʟ Pᴜsʜᴇʀ03 <153280672+pixelPusher03@users.noreply.github.com>
2025-01-05 21:49:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-token: "ISI TOKEN BOT KALIAN",
-admin: "OTHER NUMBERS OF CALIAN ID", 
+token: "7882209099:AAFP0p3darbDT39BlTpKuX6WBjoZAbfX-Qo",
+admin: "6115840198", 
 }
 module.exports = {
   ...config
